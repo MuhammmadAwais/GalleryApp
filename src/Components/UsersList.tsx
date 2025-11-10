dd6c4efc214f2df86b1bda4021681734bd622154
Awais Latif <97788862+MuhammmadAwais@users.noreply.github.com>
2025-11-10 03:12:35 -0800

    Commi the initial phase of Components

--- src/Components/UsersList.tsx
+++ src/Components/UsersList.tsx
@@ -1,11 +1,21 @@
-import React from 'react'
+import { useEffect } from "react";
+import { useDispatch   } from "react-redux";
+import type { Dispatch } from "redux";
+
+import { fetchUsers } from "../store/Thunks/fetchUsers";
 
 const UsersList: React.FC = () => {
+  const dispatch: Dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchUsers() as any);
+  }, [dispatch]);
+
   return (
     <div>
-
+      <h1>UsersList</h1>
     </div>
-  )
-}
+  );
+};
 
-export default UsersList
+export default UsersList;
