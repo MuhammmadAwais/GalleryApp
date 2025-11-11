b1c0e8118d2bc7db3d198391fc31ef7f008e8ef2
Awais Latif <97788862+MuhammmadAwais@users.noreply.github.com>
2025-11-11 16:39:55 +0500

    commit the disabling the network delay simulation

--- src/store/Thunks/fetchUsers.tsx
+++ src/store/Thunks/fetchUsers.tsx
@@ -5,13 +5,13 @@ const fetchUsers = createAsyncThunk("users/fetch", async () => {
     const response = await axios.get("http://localhost:3005/users");
 
     // Simulate network delay (ONLY FOR DEV)
-    await pause (1000)
+    // await pause (1000)
 
     return response.data
 
 });
 
 // Simulate network delay (ONLY FOR DEV)
-const pause = (duration: number) => new Promise((resolve) => setTimeout(resolve, duration));
+// const pause = (duration: number) => new Promise((resolve) => setTimeout(resolve, duration));
 
 export { fetchUsers }
